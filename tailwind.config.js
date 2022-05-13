6e818647cc0bc9842d4bd072ee31bd845472d412
Nayim hossen <nayimh2015@gmail.com>
2022-05-13 17:18:53 +0600

    navbar nad banner with react router dom

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,21 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        doctorPortal: {
+          primary: "#0FCFEC",
+          secondary: "#19D3AE",
+          accent: "#3A4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
   plugins: [require("daisyui")],
+
 }
